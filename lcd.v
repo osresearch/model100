c8aac45c9b2af5f705e7d6a97b06ae18e489b744
Trammell hudson <hudson@trmm.net>
2019-02-02 18:14:27 -0500

    lcd driver, not working yet

---
+++ lcd.v
@@ -0,0 +1,196 @@
+`ifndef _lcd_v_
+`define _lcd_v_
+
+/*
+ * Draw bitmap data to the LCD display.
+ *
+ * This draws a row at a time in auto-advance mode.
+ *
+ * Each chip handles a 50x32 rectangle and is updated 8 pixel columns
+ * at a time.  The column address auto-advances, so the address only
+ * needs to be set at the start of each column.
+ */
+
+module lcd(
+	input clk,
+	input reset,
+
+	// bitmap data to be displayed
+	input [7:0] pixels,
+	output reg [7:0] x, // 240 columns
+	output reg [3:0] y, // 8 rows of 8 pixels
+
+	// pins
+	output reg [7:0] data_pin,
+	output reg [LCD_MODULES-1:0] cs_pin,
+	output reg cs1_pin,
+	output reg rw_pin,
+	output reg di_pin,
+	output reg enable_pin,
+	output reg reset_pin
+);
+	parameter LCD_MODULES = 10;
+
+	assign rw_pin = 0; // always write
+
+	localparam STATE_INIT	= 0;
+	localparam STATE_RESET	= 1;
+	localparam STATE_ON	= 2;
+	localparam STATE_UP	= 3;
+	localparam STATE_PAGE	= 4;
+	localparam STATE_WAIT	= 5;
+	localparam STATE_WAIT1	= 6;
+	localparam STATE_WAIT2	= 7;
+	localparam STATE_WAIT3	= 8;
+	localparam STATE_DONE	= 9;
+	localparam STATE_COORD	= 10;
+	localparam STATE_COORD2 = 11;
+	localparam STATE_COORD3 = 12;
+	localparam STATE_DATA	= 13;
+	localparam STATE_DATA2  = 14;
+	localparam STATE_DATA3  = 15;
+
+	localparam MAX_X	= 240;
+	localparam X_PER_MODULE	= 50;
+
+	reg [20:0] counter;
+	reg [3:0] state;
+	reg [3:0] next_state;
+	reg [6:0] disp_x;
+
+	always @(posedge clk)
+	begin
+		counter <= counter + 1;
+
+		if (reset) begin
+			state <= STATE_INIT;
+			reset_pin <= 0; // negative logic
+			cs_pin <= 0;
+			cs1_pin <= 0;
+			x <= 0;
+			y <= 0;
+			disp_x <= 0;
+		end else
+		if (counter[2:0] != 0) begin
+			// do nothing... stretch the clocks
+		end else
+		case(state)
+		STATE_INIT: begin
+			reset_pin <= 1;
+			state <= STATE_RESET;
+			counter <= 1;
+		end
+		STATE_RESET: begin
+			// hold for a few ms
+			if (counter == 0)
+				state <= STATE_ON;
+			data_pin <= 0;
+		end
+		STATE_ON: begin
+			cs1_pin <= 1;
+			cs_pin <= ~0; // select all of them
+			di_pin <= 0;
+			data_pin <= 8'b00111001; // "Turn on display"
+			next_state <= STATE_UP;
+			state <= STATE_WAIT;
+		end
+		STATE_UP: begin
+			data_pin <= 8'b00111011; // "Up mode"
+			next_state <= STATE_PAGE;
+			state <= STATE_WAIT;
+		end
+		STATE_PAGE: begin
+			data_pin <= 8'b00111110; // "Start page 0"
+			next_state <= STATE_DONE;
+			state <= STATE_WAIT;
+		end
+		STATE_DONE: begin
+			state <= STATE_COORD;
+		end
+
+		/* Busy wait after a command */
+		STATE_WAIT: begin
+			// strobe the enable pin and wait a little while
+			enable_pin <= 1;
+			state <= STATE_WAIT2;
+		end
+		STATE_WAIT2: begin
+			enable_pin <= 0;
+			state <= STATE_WAIT3;
+			counter <= 1;
+		end
+		STATE_WAIT3: begin
+			if (counter == 0)
+				state <= next_state;
+		end
+
+		/* Framebuffer drawing code.
+		 */
+		STATE_COORD: begin
+		 	// Send all the devices to the same row/column.
+			// disp_x is ignored, since we always start at first
+			cs_pin <= ~0;
+			di_pin <= 0;
+			data_pin <= { y[1:0], 6'b000000 };
+			enable_pin <= 0;
+			state <= STATE_COORD2;
+		end
+		STATE_COORD2: begin
+			enable_pin <= 1;
+			state <= STATE_COORD3;
+		end
+
+		STATE_COORD3: begin
+			// we start on the very first module after a
+			// coordinate update.
+			cs_pin <= 1;
+			enable_pin <= 0;
+			state <= STATE_DATA;
+		end
+
+		STATE_DATA: begin
+			di_pin <= 1; // data, not instruction
+			data_pin <= pixels;
+			state <= STATE_DATA2;
+		end
+		STATE_DATA2: begin
+			// everything is stable, clock the modules
+			enable_pin <= 1;
+			state <= STATE_DATA3;
+		end
+		STATE_DATA3: begin
+			enable_pin <= 0;
+			x <= x + 1;
+			state <= STATE_DATA;
+
+			// at the end of the line?
+			if (x == MAX_X-1) begin
+				// if we're on the first row of modules,
+				// go to the second row.
+				// if we're on the second row, go to the next
+				// data page on the first row
+				if (!y[2])
+					y <= { 1'b1, y[1:0] };
+				else begin
+					y <= { 1'b0, y[1:0] + 2'b01 };
+					state <= STATE_COORD;
+				end
+
+				// swap LCD modules
+				cs_pin = { cs_pin[4:0], cs_pin[9:5] };
+				disp_x <= 0;
+				x <= 0;
+			end else
+			// at the end of this module? go to next module
+			if (disp_x == X_PER_MODULE-1) begin
+				disp_x <= 0;
+				cs_pin = { cs_pin[9-2:1], cs_pin[9-1] };
+			end
+		end
+		endcase
+	end
+
+endmodule
+
+
+`endif
--- top.v
+++ top.v
@@ -1,19 +1,113 @@
 `include "pwm.v"
+`include "lcd.v"
 
 module top(
-	output gpio_28
+	output gpio_23,
+	output gpio_25,
+	output gpio_26,
+	output gpio_27,
+	output gpio_32,
+	output gpio_35,
+	output gpio_31,
+	output gpio_37,
+	output gpio_38,
+	output gpio_28,
+	output gpio_4,
+	output gpio_44,
+	output gpio_6,
+	output gpio_9,
+	output gpio_11,
+	output gpio_18,
+	output gpio_19,
+	output gpio_13,
+	output gpio_21,
+	output gpio_12,
+	output gpio_3,
+	output gpio_48,
+	output gpio_45,
+	output gpio_47,
+	output gpio_46
 );
 
 	wire clk_48mhz;
 	SB_HFOSC ocs(1,1,clk_48mhz);
 	wire clk = clk_48mhz;
 
+	wire [7:0] lcd_x;
+	wire [3:0] lcd_y;
+	reg [63:0] framebuffer[239:0];
+	initial $readmemh("fb.hex", framebuffer);
+
+	wire [63:0] fb = framebuffer[lcd_x];
+	wire [7:0] pixels = {
+		fb[{lcd_y, 3'h0}],
+		fb[{lcd_y, 3'h1}],
+		fb[{lcd_y, 3'h2}],
+		fb[{lcd_y, 3'h3}],
+		fb[{lcd_y, 3'h4}],
+		fb[{lcd_y, 3'h5}],
+		fb[{lcd_y, 3'h6}],
+		fb[{lcd_y, 3'h7}]
+	};
+
+	wire [7:0] lcd_data = {
+		gpio_23,
+		gpio_25,
+		gpio_26,
+		gpio_27,
+		gpio_32,
+		gpio_35,
+		gpio_31,
+		gpio_37
+	};
+
+	wire [9:0] lcd_cs = {
+		gpio_12,
+		gpio_21,
+		gpio_13,
+		gpio_19,
+		gpio_18,
+
+		gpio_11,
+		gpio_9,
+		gpio_6,
+		gpio_44,
+		gpio_4
+	};
+
+	wire lcd_reset = gpio_3;
+	wire lcd_cs1 = gpio_48;
+	wire lcd_enable = gpio_45;
+	wire lcd_rw = gpio_47;
+	wire lcd_di = gpio_46;
+
+	lcd modell100_lcd(
+		.clk(clk),
+		.reset(0),
+		.pixels(pixels),
+		.x(lcd_x),
+		.y(lcd_y),
+		.data_pin(lcd_data),
+		.cs_pin(lcd_cs),
+		.cs1_pin(lcd_cs1),
+		.rw_pin(lcd_rw),
+		.di_pin(lcd_di),
+		.enable_pin(lcd_enable),
+		.reset_pin(lcd_reset)
+	);
+
 	// generate a 1/4 duty cycle wave for the
 	// negative voltage charge pump circuit
-
 	pwm negative_charge_pump(
 		.clk(clk),
 		.duty(64),
 		.out(gpio_28)
+	);
+
+	// contrast display at 1/2 duty cycle
+	pwm contrast(
+		.clk(clk),
+		.duty(2),
+		.out(gpio_38)
 	);
 endmodule
